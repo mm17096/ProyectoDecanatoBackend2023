10cfa93a615db0f95d1efcb15bad9bec1a722837
walterEDR <dr13018@ues.edu.sv>
2023-08-08 15:32:57 -0600

    arreglo de errores

--- src/main/java/com/ues/edu/apidecanatoce/services/ICRUD.java
+++ src/main/java/com/ues/edu/apidecanatoce/services/ICRUD.java
@@ -2,22 +2,17 @@ package com.ues.edu.apidecanatoce.services;
 
 import java.util.List;
 
-<<<<<<< HEAD
+
 public interface ICRUD<T>{
 
+     T registrar(T obj);
 
-  T registrar(T obj);
-=======
-public interface ICRUD<T> {
-    T registrar(T obj);
->>>>>>> walterEDR
     T modificar(T obj);
     List<T> listar();
     T leerPorId(Integer id);
     boolean eliminar(T obj);
 
-<<<<<<< HEAD
-=======
+
     List<T> listarPorEstado(int estado);
->>>>>>> walterEDR
+
 }
