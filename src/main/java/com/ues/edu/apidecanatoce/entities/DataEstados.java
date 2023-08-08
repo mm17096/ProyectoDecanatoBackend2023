dfe8125e0bdf3f8331fd3c0cc20939e31f658f41
mm17096 <mm17096@ues.edu.sv>
2023-08-08 11:35:15 -0600

    cambios controlador

--- src/main/java/com/ues/edu/apidecanatoce/entities/DataEstados.java
+++ src/main/java/com/ues/edu/apidecanatoce/entities/DataEstados.java
@@ -17,12 +17,29 @@ public class DataEstados implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Aquí puedes agregar código para insertar datos en la tabla
-        Estados estado1 = new Estados(0,"Estado 1");
-        Estados estado2 = new Estados(0,"Estado 2");
+        Estados estado1 = new Estados(0,"En espera");
+        Estados estado2 = new Estados(0,"Por aprobar");
+        Estados estado3 = new Estados(0,"Aprobada");
+        Estados estado4 = new Estados(0,"Asignado");
+        Estados estado5 = new Estados(0,"Revision");
+        Estados estado6 = new Estados(0,"Finalizada");
+        Estados estado7 = new Estados(0,"Activo");
+        Estados estado8 = new Estados(0,"Inactivo");
+        Estados estado9 = new Estados(0,"Caducado");
+        Estados estado10 = new Estados(0,"Consumido");
+        Estados estado11 = new Estados(0,"Devuelto");
 
         estadosRepository.save(estado1);
         estadosRepository.save(estado2);
+        estadosRepository.save(estado3);
+        estadosRepository.save(estado4);
+        estadosRepository.save(estado5);
+        estadosRepository.save(estado6);
+        estadosRepository.save(estado7);
+        estadosRepository.save(estado8);
+        estadosRepository.save(estado9);
+        estadosRepository.save(estado10);
+        estadosRepository.save(estado11);
     }
 }
 
