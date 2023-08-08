867ac63bf81f98541fa5c8419cc7a114b700c75c
walterEDR <dr13018@ues.edu.sv>
2023-08-08 15:21:40 -0600

    correcion en icrud

--- src/main/java/com/ues/edu/apidecanatoce/servicesImpl/DeptoServiceImp.java
+++ src/main/java/com/ues/edu/apidecanatoce/servicesImpl/DeptoServiceImp.java
@@ -44,6 +44,11 @@ public class DeptoServiceImp implements IDeptoService {
     }
 
     @Override
+    public List<Departamento> listarPorEstado(int estado) {
+        return null;
+    }
+
+    @Override
     public Departamento leerPorId(Integer id) {
         return this.deptopService.findById(id).orElse(new Departamento());
     }
