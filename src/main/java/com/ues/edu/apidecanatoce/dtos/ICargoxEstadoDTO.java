6c7ade779a45f4ebdad25515e8d28dfe70bf10cb
walterEDR <dr13018@ues.edu.sv>
2023-08-20 14:45:15 -0600

    metodos de busqueda en Cargo

---
+++ src/main/java/com/ues/edu/apidecanatoce/dtos/ICargoxEstadoDTO.java
@@ -0,0 +1,14 @@
+package com.ues.edu.apidecanatoce.dtos;
+
+import lombok.*;
+
+
+public interface ICargoxEstadoDTO {
+
+    String getNombreCargo();
+
+    String getDescripcion();
+
+    String getNomEstado();
+
+}
