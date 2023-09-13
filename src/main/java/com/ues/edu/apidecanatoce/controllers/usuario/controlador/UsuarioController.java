0aaa5a44d5f6757cc3339e900a90022f3e832128
mm17096 <mm17096@ues.edu.sv>
2023-09-13 09:13:59 -0600

    Correcciones en sesion y validaciones de usuario

--- src/main/java/com/ues/edu/apidecanatoce/controllers/usuario/controlador/UsuarioController.java
+++ src/main/java/com/ues/edu/apidecanatoce/controllers/usuario/controlador/UsuarioController.java
@@ -1,13 +1,11 @@
 package com.ues.edu.apidecanatoce.controllers.usuario.controlador;
 
-import com.ues.edu.apidecanatoce.dtos.empleados.EmpleadoPeticionDto;
 import com.ues.edu.apidecanatoce.dtos.usuario.UsuarioPeticionDto;
-import com.ues.edu.apidecanatoce.services.usuario.IUsuarioService;
+import com.ues.edu.apidecanatoce.entities.usuario.Usuario;
+import com.ues.edu.apidecanatoce.repositorys.usuario.IUsuarioRepository;
 import com.ues.edu.apidecanatoce.servicesImpl.usuario.UsuarioServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,13 +13,16 @@ import java.util.UUID;
 public class UsuarioController {
     private final UsuarioServiceImpl usuarioService;
 
+
+
     public UsuarioController(UsuarioServiceImpl usuarioService) {
         this.usuarioService = usuarioService;
     }
-
 
     @GetMapping("/{uuid}")
     public ResponseEntity<UsuarioPeticionDto> leerPorID(@PathVariable String uuid) {
         return ResponseEntity.ok(usuarioService.leerPorID(uuid));
     }
+
+
 }
