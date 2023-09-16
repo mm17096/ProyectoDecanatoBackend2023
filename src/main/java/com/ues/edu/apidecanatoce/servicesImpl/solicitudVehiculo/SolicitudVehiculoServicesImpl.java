4e1342c2f81356d9717bf9069b0ee540d7a88bc3
Victor Alonso <aa14037@ues.edu.sv>
2023-09-15 22:07:00 -0600

    fix documentos solicitud vehiculo

--- src/main/java/com/ues/edu/apidecanatoce/servicesImpl/solicitudVehiculo/SolicitudVehiculoServicesImpl.java
+++ src/main/java/com/ues/edu/apidecanatoce/servicesImpl/solicitudVehiculo/SolicitudVehiculoServicesImpl.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
