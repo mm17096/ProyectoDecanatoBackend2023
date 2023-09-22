15c1fa07e672bc027e2000681b5b4a524cd41dc6
Absalon2327 <cm16057@ues.edu.sv>
2023-09-21 23:41:02 -0600

    estado y consulta para vales a asignar

--- src/main/java/com/ues/edu/apidecanatoce/dtos/solicitudValeDto/SolicitudValeADto.java
+++ src/main/java/com/ues/edu/apidecanatoce/dtos/solicitudValeDto/SolicitudValeADto.java
@@ -1,4 +1,5 @@
 package com.ues.edu.apidecanatoce.dtos.solicitudValeDto;
+
 import com.ues.edu.apidecanatoce.entities.solicitudVale.SolicitudVale;
 import com.ues.edu.apidecanatoce.entities.solicitudVehiculo.SolicitudVehiculo;
 import com.ues.edu.apidecanatoce.exceptions.CustomException;
@@ -12,6 +13,7 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
+
 @Getter
 @Setter
 @Builder
@@ -22,6 +24,9 @@ public class SolicitudValeADto {
     private Integer cantidadVale;
 
     private Integer estadoEntrada;
+
+    private Integer estado;
+
     @NotNull(message = "Solicitud de vehiculo es obligatoria")
     private UUID solicitudVehiculo;
 
@@ -29,6 +34,7 @@ public class SolicitudValeADto {
         SolicitudVehiculo solicitudVehiculobuscar = SolicitudVehiculo.findById(this.solicitudVehiculo).orElseThrow(
                 () -> new CustomException(HttpStatus.NOT_FOUND, "No se encuentro compra"));
         return SolicitudVale.builder().idSolicitudVale(this.idSolicitudVale).cantidadVale(this.cantidadVale)
-                .estadoEntrada(this.estadoEntrada).solicitudVehiculo(solicitudVehiculobuscar).build();
+                .estadoEntrada(this.estadoEntrada).solicitudVehiculo(solicitudVehiculobuscar).estado(this.estado).
+                build();
     }
 }
