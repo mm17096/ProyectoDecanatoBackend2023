db59d4ce594e2d64df20419ad6916459153f6e15
walterEDR <dr13018@ues.edu.sv>
2023-10-26 16:21:12 -0600

    validacion de cambio de correo

--- src/main/java/com/ues/edu/apidecanatoce/servicesImpl/usuario/UsuarioServiceImpl.java
+++ src/main/java/com/ues/edu/apidecanatoce/servicesImpl/usuario/UsuarioServiceImpl.java
@@ -165,7 +165,7 @@ no se esta usando
     }
 
     // codigo agregado de prueba
-    public void modificar(RegisterRequest request, Empleado empleado) {
+    public void modificar(RegisterRequest request, Empleado empleado, String correoActual) {
 
         System.out.println("ingreso al modificar");
         Role rol;
@@ -209,7 +209,12 @@ no se esta usando
         String[] partes = request.getNombre().split("@");
         String nombre = partes[0];
 
-        Usuario carga = usuarioRepository.findUsuarioByNombre(nombre);
+        String[] partes2 = correoActual.split("@");
+        String nombre2 = partes2[0];
+
+
+
+        Usuario carga = usuarioRepository.findUsuarioByNombre(nombre2);
 
         /*
         Usuario user = Usuario.builder()
@@ -224,6 +229,7 @@ no se esta usando
                 .token(carga.getToken())
                 .build();
 */
+                carga.setNombre(nombre);
                 carga.setRole(rol);
         usuarioRepository.save(carga);
 
